950c4a439aff7270e143a984ac1b768ef07d8f71
AJ Morris <ajmorris2002@gmail.com>
2017-07-29 15:35:49 -0400

    playing with edd's settings options, but it's not working.

--- lib/addon-settings.php
+++ lib/addon-settings.php
@@ -42,7 +42,7 @@ class IT_Exchange_Customer_History_Add_On {
 	 * @var string $statcanadian_message will be displayed if not empty
 	 * @since 1.0.0
 	*/
-	var $statcanadian_message;
+	var $status_message;
 
 	/**
 	 * @var string $error_message will be displayed if not empty
@@ -63,9 +63,15 @@ class IT_Exchange_Customer_History_Add_On {
 		$this->_current_add_on = empty( $_GET['add-on-settings'] ) ? false : $_GET['add-on-settings'];
 
 		if ( ! empty( $_POST ) && $this->_is_admin && 'it-exchange-addons' == $this->_current_page && 'customer-history' == $this->_current_add_on ) {
-			add_action( 'it_exchange_save_add_on_settings_customer_history', array( $this, 'save_settings' ) );
-			do_action( 'it_exchange_save_add_on_settings_customer_history' );
+			// add_action( 'it_exchange_save_add_on_settings_customer_history', array( $this, 'save_settings' ) );
+      add_action('it_exchange_save_add_on_settings_customer_history', array( $this, 'exchange_customer_history_license_deactivate' ) );
+      add_action('it_exchange_save_add_on_settings_customer_history', array( $this, 'exchange_customer_history_license_activate' ) );
+      add_action('it_exchange_save_add_on_settings_customer_history', array( $this, 'exchange_customer_history_register_option' ) );
+    	do_action( 'it_exchange_save_add_on_settings_customer_history' );
 		}
+
+
+
 	}
 
 	/**
@@ -80,245 +86,235 @@ class IT_Exchange_Customer_History_Add_On {
 	}
 
 	function print_settings_page() {
-		$settings = it_exchange_get_option( 'addon_customer_history', true, false );
+    $license = get_option( 'exchange_customer_history_license_key' );
+  	$status  = get_option( 'exchange_customer_history_license_status' );
+  	?>
+    <div class="wrap">
+      <?php screen_icon( 'it-exchange' ); ?>
+  		<h2><?php _e('ExchangeWP Customer History License Options'); ?></h2>
+  		<form method="post" action="options.php">
 
-		$form_values  = empty( $this->error_message ) ? $settings : ITForm::get_post_data();
-		$form_options = array(
-			'id'      => apply_filters( 'it_exchange_add_on_customer_history', 'it-exchange-add-on-customer-history-settings' ),
-			'enctype' => apply_filters( 'it_exchange_add_on_customer_history_settings_form_enctype', false ),
-			'action'  => 'admin.php?page=it-exchange-addons&add-on-settings=customer_history',
-		);
-		$form         = new ITForm( $form_values, array( 'prefix' => 'it-exchange-add-on-customer-history' ) );
+  			<?php settings_fields('exchange_customer_history_license'); ?>
 
-		if ( ! empty ( $this->status_message ) )
-			ITUtility::show_status_message( $this->status_message );
-		if ( ! empty( $this->error_message ) )
-			ITUtility::show_error_message( $this->error_message );
+  			<table class="form-table">
+  				<tbody>
+  					<tr valign="top">
+  						<th scope="row" valign="top">
+  							<?php _e('License Key'); ?>
+  						</th>
+  						<td>
+  							<input id="exchange_customer_history_license_key" name="exchange_customer_history_license_key" type="text" class="regular-text" value="<?php esc_attr_e( $license ); ?>" />
+  							<label class="description" for="exchange_customer_history_license_key"><?php _e('Enter your license key'); ?></label>
+  						</td>
+  					</tr>
+  					<?php if( false !== $license ) { ?>
+  						<tr valign="top">
+  							<th scope="row" valign="top">
+  								<?php _e('Activate License'); ?>
+  							</th>
+  							<td>
+  								<?php if( $status !== false && $status == 'valid' ) { ?>
+  									<span style="color:green;"><?php _e('active'); ?></span>
+  									<?php wp_nonce_field( 'exchange_customer_history_nonce', 'exchange_customer_history_nonce' ); ?>
+  									<input type="submit" class="button-secondary" name="exchange_customer_history_deactivate" value="<?php _e('Deactivate License'); ?>"/>
+  								<?php } else {
+  									wp_nonce_field( 'exchange_customer_history_nonce', 'exchange_customer_history_nonce' ); ?>
+  									<input type="submit" class="button-secondary" name="exchange_customer_history_license_activate" value="<?php _e('Activate License'); ?>"/>
+  								<?php } ?>
+  							</td>
+  						</tr>
+  					<?php } ?>
+  				</tbody>
+  			</table>
+  			<?php submit_button(); ?>
 
-		?>
-		<div class="wrap">
-			<?php screen_icon( 'it-exchange' ); ?>
-			<h2><?php _e( 'Customer History Settings', 'LION' ); ?></h2>
-
-			<?php do_action( 'it_exchange_customer_history_settings_page_top' ); ?>
-			<?php do_action( 'it_exchange_addon_settings_page_top' ); ?>
-
-			<?php $form->start_form( $form_options, 'it-exchange-customer-history-settings' ); ?>
-				<?php do_action( 'it_exchange_customer_history_settings_form_top' ); ?>
-				<h4>License Key</h4>
-				<?php
-				   $exchangewp_customer_history_options = get_option( 'it-storage-exchange_addon_customer_history' );
-				   $license = $exchangewp_customer_history_options['customer_history_license'];
-				   // var_dump($license);
-				   $exstatus = trim( get_option( 'exchange_customer_history_license_status' ) );
-				   // var_dump($exstatus);
-				?>
-				<p>
-				 <label class="description" for="exchange_customer_history_license_key"><?php _e('Enter your license key'); ?></label>
-				 <!-- <input id="customer_history_license" name="it-exchange-add-on-customer_history-customer_history_license" type="text" value="<?php #esc_attr_e( $license ); ?>" /> -->
-				 <?php $form->add_text_box( 'customer_history_license' ); ?>
-				 <span>
-				   <?php if( $exstatus !== false && $exstatus == 'valid' ) { ?>
-							<span style="color:green;"><?php _e('active'); ?></span>
-							<?php wp_nonce_field( 'exchange_customer_history_nonce', 'exchange_customer_history_nonce' ); ?>
-							<input type="submit" class="button-secondary" name="exchange_customer_history_license_deactivate" value="<?php _e('Deactivate License'); ?>"/>
-						<?php } else {
-							wp_nonce_field( 'exchange_customer_history_nonce', 'exchange_customer_history_nonce' ); ?>
-							<input type="submit" class="button-secondary" name="exchange_customer_history_license_activate" value="<?php _e('Activate License'); ?>"/>
-						<?php } ?>
-				 </span>
-				</p>
-
-
-				<?php do_action( 'it_exchange_customer_history_settings_form_bottom' ); ?>
-				<p class="submit">
-					<?php $form->add_submit( 'submit', array( 'value' => __( 'Save Changes', 'LION' ), 'class' => 'button button-primary button-large' ) ); ?>
-				</p>
-			<?php $form->end_form(); ?>
-			<?php do_action( 'it_exchange_customer_history_settings_page_bottom' ); ?>
-			<?php do_action( 'it_exchange_addon_settings_page_bottom' ); ?>
-		</div>
-		<?php
+  		</form>
+  	<?php
 	}
 
-	/**
-	 * Save settings
-	 *
-	 * @since 1.0.0
-	 * @return void
-	*/
-    function save_settings() {
-        $defaults = it_exchange_get_option( 'addon_customer_history' );
-        $new_values = wp_parse_args( ITForm::get_post_data(), $defaults );
 
-        // Check nonce
-    		if ( ! wp_verify_nonce( $_POST['_wpnonce'], 'it-exchange-customer-history-settings' ) ) {
-    			$this->error_message = __( 'Error. Please try again', 'LION' );
-    			return;
-    		}
+  function exchange_customer_history_register_option() {
+  	// creates our settings in the options table
+  	register_setting('exchange_customer_history_license', 'exchange_customer_history_license_key', 'exchange_sanatize_license' );
+  }
 
-    		$errors = apply_filters( 'it_exchange_add_on_manual_transaction_validate_settings', $this->get_form_errors( $new_values ), $new_values );
-    		if ( ! $errors && it_exchange_save_option( 'addon_customer_history', $new_values ) ) {
-    			ITUtility::show_status_message( __( 'Settings saved.', 'LION' ) );
-    		} else if ( $errors ) {
-    			$errors = implode( '<br />', $errors );
-    			$this->error_message = $errors;
-    		} else {
-    			$this->status_message = __( 'Settings not saved.', 'LION' );
-    		}
-
-				if( isset( $_POST['exchange_customer_history_license_activate'] ) ) {
-
-					// run a quick security check
-				 	if( ! check_admin_referer( 'exchange_customer_history_nonce', 'exchange_customer_history_nonce' ) )
-						return; // get out if we didn't click the Activate button
-
-					// retrieve the license from the database
-					// $license = trim( get_option( 'exchange_customer_history_license_key' ) );
-					$exchangewp_customer_history_options = get_option( 'it-storage-exchange_addon_customer_history' );
- 				  $license = $exchangewp_customer_history_options['customer_history_license'];
-
-					// data to send in our API request
-					$api_params = array(
-						'edd_action' => 'activate_license',
-						'license'    => $license,
-						'item_name'  => urlencode( 'customer-history' ), // the name of our product in EDD
-						'url'        => home_url()
-					);
-
-					// Call the custom API.
-					$response = wp_remote_post( 'https://exchangewp.com', array( 'timeout' => 15, 'sslverify' => false, 'body' => $api_params ) );
-
-					// make sure the response came back okay
-					if ( is_wp_error( $response ) || 200 !== wp_remote_retrieve_response_code( $response ) ) {
-
-						if ( is_wp_error( $response ) ) {
-							$message = $response->get_error_message();
-						} else {
-							$message = __( 'An error occurred, please try again.' );
-						}
-
-					} else {
-
-						$license_data = json_decode( wp_remote_retrieve_body( $response ) );
-
-						if ( false === $license_data->success ) {
-
-							switch( $license_data->error ) {
-
-								case 'expired' :
-
-									$message = sprintf(
-										__( 'Your license key expired on %s.' ),
-										date_i18n( get_option( 'date_format' ), strtotime( $license_data->expires, current_time( 'timestamp' ) ) )
-									);
-									break;
-
-								case 'revoked' :
-
-									$message = __( 'Your license key has been disabled.' );
-									break;
-
-								case 'missing' :
-
-									$message = __( 'Invalid license.' );
-									break;
-
-								case 'invalid' :
-								case 'site_inactive' :
-
-									$message = __( 'Your license is not active for this URL.' );
-									break;
-
-								case 'item_name_mismatch' :
-
-									$message = sprintf( __( 'This appears to be an invalid license key for %s.' ), 'customer_history' );
-									break;
-
-								case 'no_activations_left':
-
-									$message = __( 'Your license key has reached its activation limit.' );
-									break;
-
-								default :
-
-									$message = __( 'An error occurred, please try again.' );
-									break;
-							}
-
-						}
-
-					}
-
-					// Check if anything passed on a message constituting a failure
-					if ( ! empty( $message ) ) {
-						$base_url = admin_url( 'admin.php?page=' . 'customer_history' );
-						$redirect = add_query_arg( array( 'sl_activation' => 'false', 'message' => urlencode( $message ) ), $base_url );
-
-						wp_redirect( $redirect );
-						exit();
-					}
-
-					//$license_data->license will be either "valid" or "invalid"
-					update_option( 'exchange_customer_history_license_status', $license_data->license );
-					// wp_redirect( admin_url( 'admin.php?page=' . 'customer_history-license' ) );
-					exit();
-				}
-
-			 // deactivate here
-			 // listen for our activate button to be clicked
-				if( isset( $_POST['exchange_customer_history_license_deactivate'] ) ) {
-
-					// run a quick security check
-				 	if( ! check_admin_referer( 'exchange_customer_history_nonce', 'exchange_customer_history_nonce' ) )
-						return; // get out if we didn't click the Activate button
-
-					// retrieve the license from the database
-					// $license = trim( get_option( 'exchange_customer_history_license_key' ) );
-
-					$exchangewp_customer_history_options = get_option( 'it-storage-exchange_addon_customer_history' );
- 				  $license = $exchangewp_customer_history_options['customer_history_license'];
+  function exchange_sanatize_license( $new ) {
+  	$old = get_option( 'exchange_customer_history_license_key' );
+  	if( $old && $old != $new ) {
+  		delete_option( 'exchange_customer_history_license_status' ); // new license has been entered, so must reactivate
+  	}
+  	return $new;
+  }
 
 
-					// data to send in our API request
-					$api_params = array(
-						'edd_action' => 'deactivate_license',
-						'license'    => $license,
-						'item_name'  => urlencode( 'customer-history' ), // the name of our product in EDD
-						'url'        => home_url()
-					);
-					// Call the custom API.
-					$response = wp_remote_post( 'https://exchangewp.com', array( 'timeout' => 15, 'sslverify' => false, 'body' => $api_params ) );
 
-					// make sure the response came back okay
-					if ( is_wp_error( $response ) || 200 !== wp_remote_retrieve_response_code( $response ) ) {
+  /************************************
+  * this illustrates how to activate
+  * a license key
+  *************************************/
 
-						if ( is_wp_error( $response ) ) {
-							$message = $response->get_error_message();
-						} else {
-							$message = __( 'An error occurred, please try again.' );
-						}
+  function exchange_customer_history_license_activate() {
 
-						// $base_url = admin_url( 'admin.php?page=' . 'customer_history-license' );
-						// $redirect = add_query_arg( array( 'sl_activation' => 'false', 'message' => urlencode( $message ) ), $base_url );
+  	// listen for our activate button to be clicked
+  	if( isset( $_POST['exchange_customer_history_license_activate'] ) ) {
 
-						wp_redirect( 'admin.php?page=customer_history' );
-						exit();
-					}
+  		// run a quick security check
+  	 	if( ! check_admin_referer( 'exchange_customer_history_nonce', 'exchange_customer_history_nonce' ) )
+  			return; // get out if we didn't click the Activate button
 
-					// decode the license data
-					$license_data = json_decode( wp_remote_retrieve_body( $response ) );
-					// $license_data->license will be either "deactivated" or "failed"
-					if( $license_data->license == 'deactivated' ) {
-						delete_option( 'exchange_customer_history_license_status' );
-					}
+  		// retrieve the license from the database
+  		$license = trim( get_option( 'exchange_customer_history_license_key' ) );
 
-					// wp_redirect( admin_url( 'admin.php?page=' . 'customer_history-license' ) );
-					exit();
 
-				}
+  		// data to send in our API request
+  		$api_params = array(
+  			'edd_action' => 'activate_license',
+  			'license'    => $license,
+  			'item_name'  => urlencode( 'customer-history' ), // the name of our product in EDD
+  			'url'        => home_url()
+  		);
 
-			}
+  		// Call the custom API.
+  		$response = wp_remote_post( 'https://exchangewp.com', array( 'timeout' => 15, 'sslverify' => false, 'body' => $api_params ) );
+
+  		// make sure the response came back okay
+  		if ( is_wp_error( $response ) || 200 !== wp_remote_retrieve_response_code( $response ) ) {
+
+  			if ( is_wp_error( $response ) ) {
+  				$message = $response->get_error_message();
+  			} else {
+  				$message = __( 'An error occurred, please try again.' );
+  			}
+
+  		} else {
+
+  			$license_data = json_decode( wp_remote_retrieve_body( $response ) );
+
+  			if ( false === $license_data->success ) {
+
+  				switch( $license_data->error ) {
+
+  					case 'expired' :
+
+  						$message = sprintf(
+  							__( 'Your license key expired on %s.' ),
+  							date_i18n( get_option( 'date_format' ), strtotime( $license_data->expires, current_time( 'timestamp' ) ) )
+  						);
+  						break;
+
+  					case 'revoked' :
+
+  						$message = __( 'Your license key has been disabled.' );
+  						break;
+
+  					case 'missing' :
+
+  						$message = __( 'Invalid license.' );
+  						break;
+
+  					case 'invalid' :
+  					case 'site_inactive' :
+
+  						$message = __( 'Your license is not active for this URL.' );
+  						break;
+
+  					case 'item_name_mismatch' :
+
+  						$message = sprintf( __( 'This appears to be an invalid license key for %s.' ), 'customer-history' );
+  						break;
+
+  					case 'no_activations_left':
+
+  						$message = __( 'Your license key has reached its activation limit.' );
+  						break;
+
+  					default :
+
+  						$message = __( 'An error occurred, please try again.' );
+  						break;
+  				}
+
+  			}
+
+  		}
+
+  		// Check if anything passed on a message constituting a failure
+  		if ( ! empty( $message ) ) {
+  			$base_url = admin_url( 'admin.php?page=' . 'customer-history' );
+  			$redirect = add_query_arg( array( 'sl_activation' => 'false', 'message' => urlencode( $message ) ), $base_url );
+
+  			wp_redirect( $redirect );
+  			exit();
+  		}
+
+  		// $license_data->license will be either "valid" or "invalid"
+
+  		update_option( 'exchange_customer_history_license_status', $license_data->license );
+  		wp_redirect( admin_url( 'admin.php?page=' . 'customer-history' ) );
+  		exit();
+  	}
+  }
+
+
+
+  /***********************************************
+  * Illustrates how to deactivate a license key.
+  * This will decrease the site count
+  ***********************************************/
+
+  function exchange_customer_history_license_deactivate() {
+
+  	// listen for our activate button to be clicked
+  	if( isset( $_POST['exchange_customer_history_deactivate'] ) ) {
+
+  		// run a quick security check
+  	 	if( ! check_admin_referer( 'exchange_customer_history_nonce', 'exchange_customer_history_nonce' ) )
+  			return; // get out if we didn't click the Activate button
+
+  		// retrieve the license from the database
+  		$license = trim( get_option( 'exchange_customer_history_license_key' ) );
+
+
+  		// data to send in our API request
+  		$api_params = array(
+  			'edd_action' => 'deactivate_license',
+  			'license'    => $license,
+  			'item_name'  => urlencode( 'customer-history' ), // the name of our product in EDD
+  			'url'        => home_url()
+  		);
+
+  		// Call the custom API.
+  		$response = wp_remote_post( 'https://exchangewp.com', array( 'timeout' => 15, 'sslverify' => false, 'body' => $api_params ) );
+
+  		// make sure the response came back okay
+  		if ( is_wp_error( $response ) || 200 !== wp_remote_retrieve_response_code( $response ) ) {
+
+  			if ( is_wp_error( $response ) ) {
+  				$message = $response->get_error_message();
+  			} else {
+  				$message = __( 'An error occurred, please try again.' );
+  			}
+
+  			$base_url = admin_url( 'plugins.php?page=' . 'customer-history' );
+  			$redirect = add_query_arg( array( 'sl_activation' => 'false', 'message' => urlencode( $message ) ), $base_url );
+
+  			wp_redirect( $redirect );
+  			exit();
+  		}
+
+  		// decode the license data
+  		$license_data = json_decode( wp_remote_retrieve_body( $response ) );
+
+  		// $license_data->license will be either "deactivated" or "failed"
+  		if( $license_data->license == 'deactivated' ) {
+  			delete_option( 'exchange_customer_history_license_status' );
+  		}
+
+  		wp_redirect( admin_url( 'plugins.php?page=' . 'customer-history' ) );
+  		exit();
+
+  	}
+  }
 
 		/**
 		* This is a means of catching errors from the activation method above and displaying it to the customer
